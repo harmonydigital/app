d62e338aa01230f13c3505097c5a9036baf3a8b2
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-08-23 21:36:03 -0300

    Add files via upload

--- lib.js
+++ lib.js
@@ -1,10 +1,34 @@
 let whatBtn=document.querySelectorAll('.whats-btn')
+let formMsg=document.querySelector('.formMsg')
+let leadMsg=document.getElementById('leadMsg')
 
-// console.log(whatBtn)
+ 
 
-Array.from(whatBtn).map((el)=>{
-    console.log(el)
-    el.onclick=()=>{
-        location.href="https://api.whatsapp.com/send/?phone=5511918419084&text=Ol%C3%A1+Harmony+Digital"
+openWhat=()=>{
+    return location.href="https://api.whatsapp.com/send/?phone=5511918419084&text=Ol%C3%A1+Harmony+Digital"
+}
+sendMsg=()=>{ 
+    
+    return location.href="https://api.whatsapp.com/send/?phone=5511918419084&text=Ol%C3%A1+Harmony+Digital+"+leadMsg.value
+}
+
+Array.from(whatBtn).map((el)=>{ 
+    
+    el.onclick=(e)=>{  
+    e.preventDefault() 
+ 
+
+     leadMsg.value.length.length>0?openWhat():sendMsg() 
+
+
     }
+})
+
+formMsg.addEventListener('submit',(e)=>{
+    
+    e.preventDefault()  
+    console.log(leadMsg.value.length) 
+
+    leadMsg.value.length.length>0?openWhat():sendMsg() 
+
 })
